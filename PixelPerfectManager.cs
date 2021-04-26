1f4a93baf9ecc3852dc8abb1ad3557987cd7c33f
JanuZz <72662150+JanuZz@users.noreply.github.com>
2021-04-26 13:11:46 +0200

    initial commit

---
+++ PixelPerfectManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PixelPerfectManager : MonoBehaviour
+{
+    public Shader Shader;
+    [Range((float) 0.01,1)]
+    public double Resolution;
+    Material PixelMat;
+
+    private void Start()
+    {
+        PixelMat = new Material(Shader);
+    }
+
+    private void OnRenderImage(RenderTexture source, RenderTexture destination)
+    {
+        PixelMat.SetFloat("_Resolution", (float) Resolution);
+        Graphics.Blit(source, destination, PixelMat);
+    }
+}
